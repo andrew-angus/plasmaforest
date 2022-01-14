18b11f3175334fdaefe106284ec86932c88c1f74
Andrew Angus <andrew.angus@warwick.ac.uk>
2022-01-14 18:30:21 +0000

    Add NRL formulary calcs for coulomb logs

--- plasmaforest/core.py
+++ plasmaforest/core.py
@@ -2,18 +2,30 @@
 
 import plasmapy as pp
 import astropy.units as u
+import scipy.constants as sc
+import numpy as np
 
 # Core class, mainly a wrapper of select plasmapy functionality
+# Currently restricted to single-ion species
+# Specifying ion parameters optional
 # Take inputs in SI units
 class forest:
   # Initialise with physical parameters and dimensionality
-  def __init__(self,Te,ne,ndim):
+  def __init__(self,Te,ne,ndim,Z=None,Ti=None,mi=None):
     self.Te = Te
     self.ne = ne
     self.ndim = ndim
+    self.Z = Z
+    self.Ti = Ti
+    self.mi = mi
     self.vthe = None
     self.ompe = None
     self.dbyl = None
+    self.coulomb_log_ei = None
+    self.coulomb_log_ee = None
+    self.coulomb_log_ii = None
+    if Z is not None:
+      self.ni = ne/Z # Quasi-neutrality
 
   # Get electron thermal velocity
   def get_vthe(self):
@@ -35,8 +47,56 @@ class forest:
     dbyl = pp.formulary.parameters.Debye_length(T_e=Te,n_e=ne)
     self.dbyl = dbyl.value
 
+  # Get coulomb logaritms
+  # Calculated using NRL formulary (which uses cgs units)
+  def get_coulomb_log(self,species):
+    # Check ion parameters specified
+    self._exist(self.Z,'self.Z')
+    self._exist(self.Ti,'self.Ti')
+    self._exist(self.mi,'self.mi')
+    self._exist(self.ni,'self.ni')
+
+    # Get everything in cgs units, eV for temperatures
+    ne = (self.ne/u.m**3).cgs.value
+    ni = (self.ni/u.m**3).cgs.value
+    Te = temperature_energy(self.Te,method='KtoeV')
+    Ti = temperature_energy(self.Ti,method='KtoeV')
+    me = (sc.m_e*u.kg).cgs.value 
+    mi = (self.mi*u.kg).cgs.value 
+    mui = self.mi/sc.m_p
+    Z = self.Z
+
+    ## Coulomb log calcs for different species
+    # Electron-ion 
+    if species == 'ei' or species == 'ie':
+      # Selective quantities
+      Ti_mr = Ti*me/mi
+      Zscaled = 10*Z**2
+      # Evaluation cases
+      if Ti_mr < Te and Te < Zscaled:
+        cl = 23-np.log(np.sqrt(ne)*Z*np.power(Te,-3/2))
+      elif Ti_mr < Zscaled and Zscaled < Te:
+        cl = 24-np.log(np.sqrt(ne)/Te)
+      elif Te < Ti_mr:
+        cl = 16-np.log(np.sqrt(ni)*np.power(Ti,-3/2)*Z**2*mui)
+      else:
+        raise Exception(\
+            "Error: coulomb_log_ei calc does not fit any NRL formulary cases") 
+      self.coulomb_log_ei = cl
+    # Electron-electron
+    elif species == 'ee':
+      self.coulomb_log_ee = 23.5-np.log(np.sqrt(ne)*np.power(Te,-5/4))\
+          -np.sqrt(1e-5+(np.log(Te)-2)**2/16)
+    # Ion-ion
+    elif species == 'ii':
+      self.coulomb_log_ii = 23-np.log(Z**2/Ti*np.sqrt(2*ni*Z**2/Ti))
+
+  def _exist(self,var,varname):
+    if var is None:
+      raise Exception(f"Error: must set {varname}")
+
 # Function for converting between eV and K using astropy
-def temperature_energy(T,method=None):
+def temperature_energy(T,method):
   if method == 'KtoeV':
     T = T * u.K
     TeV = T.to(u.eV,equivalencies=u.temperature_energy())
--- validation/forest_test.py
+++ validation/forest_test.py
@@ -10,8 +10,9 @@
 from plasmaforest.core import *
 import scipy.constants as sc
 from numpy import pi
+import numpy as np
 import astropy.units as u
-from plasmapy.formulary.parameters import thermal_speed
+from plasmapy.formulary.collisions import Coulomb_logarithm
 from plasmapy.utils.exceptions import RelativityWarning
 import warnings
 
@@ -25,11 +26,14 @@ laser_kvac = 2*pi/laser_lambda
 laser_omega = sc.c*laser_kvac
 nc = sc.epsilon_0*sc.m_e/sc.e**2*laser_omega**2 # m/s
 ne = 0.1*nc # plasma at 0.1 nc
-Te = 3000 # eV
-Te = temperature_energy(Te,'eVtoK')
+TeeV = 3000 # eV
+Te = temperature_energy(TeeV,'eVtoK')
+Z = 1.5
+Ti = Te/3
+mi = 2.5*sc.m_p
 
 # Get forest class instance and assert setup
-birch = forest(Te,ne,ndim)
+birch = forest(Te,ne,ndim,Z,Ti,mi)
 print('Temp {K}: %0.1f; Density {1/m^3}: %0.3e; ndim: %d' \
     % (birch.Te,birch.ne,birch.ndim))
 necheck = 9.049e26
@@ -38,7 +42,6 @@ real_assert(birch.ne,necheck,1e23)
 real_assert(birch.Te,Techeck,1e4)
 
 ## Calculate quantities and validate
-
 # Electron thermal velocity
 birch.get_vthe()
 vthe_check = 22.97*1e-6*1e12
@@ -56,6 +59,12 @@ birch.get_dbyl()
 dbyl_check = 13.54e-9
 print('\lambda_D [m]: %0.3e' % (birch.dbyl))
 real_assert(birch.dbyl,dbyl_check,1e-11)
+
+# Coulomb logarithm
+birch.get_coulomb_log(species='ei')
+cl_check = 7.88
+print('\lambda_{ei}: %0.2f' % (birch.coulomb_log_ei))
+real_assert(birch.coulomb_log_ei,cl_check,1e-2)
 
 # Final statement
 print('All tests in forest_test.py complete.\n')
